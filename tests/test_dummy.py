5cb37ae8d7534c7e101e81d1bf522940dfe63ea7
aldcha <aldcha@eltiempo.com>
2025-05-09 19:25:14 -0500

    Prueba4

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def test_data_loading():
-    df = pd.read_csv("data/Life Expectancy Data.csv")
+    df = pd.read_csv("data/Life_Expectancy_Data.csv")
     assert not df.empty, "El archivo CSV está vacío"
